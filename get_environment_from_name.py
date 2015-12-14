a548183ef4b7122c486ee38e4e017de284a40e3b
Victor Estrade <victor.antoine.estrade@gmail.fr>
2015-12-14 16:16:48 +0100

    PEP8

--- get_environment_from_name.py
+++ get_environment_from_name.py
@@ -1,21 +1,22 @@
 import environments
 
+
 def get_environment_from_name(env_name):
     """ Initialize environments with default parameters, given a name.
-    
+
     Arguments:
         env_name - Name of the environment
-        
+
     Returns:
         an environment of class Environment.
     """
-    
+
     if env_name == 'FlipCoin':
         import mdps
         from mdps.mdp_flip_coins import MDPFlipCoin
-        from environments.env_with_mdp import EnvWithMDP        
+        from environments.env_with_mdp import EnvWithMDP
         return EnvWithMDP(MDPFlipCoin())
-        
+
     elif env_name == 'FlipTwoCoins':
         from mdps.mdp_flip_coins import MDPFlipTwoCoins
         from environments.env_with_mdp import EnvWithMDP
@@ -28,23 +29,22 @@ def get_environment_from_name(env_name):
         n_cols = 5
         stochasticity = 0.0
         discount = 1.0
-        mdp = MDPGrid(n_rows,n_cols,stochasticity,discount)
+        mdp = MDPGrid(n_rows, n_cols, stochasticity, discount)
         return EnvWithMDP(mdp)
-        
+
     elif env_name == 'Maze':
         from environments.env_maze import EnvMaze
         char_grid = [
-            ['T','.','.','.','.'],
-            ['W','W','W','.','W'],
-            ['.','.','W','.','.'],
-            ['.','W','W','.','.'],
-            ['.','.','.','.','.'],
+            ['T', '.', '.', '.', '.'],
+            ['W', 'W', 'W', '.', 'W'],
+            ['.', '.', 'W', '.', '.'],
+            ['.', 'W', 'W', '.', '.'],
+            ['.', '.', '.', '.', '.'],
         ]
-        stochasticity=0.1
-        bump_penalty=-10
-        return EnvMaze(char_grid,stochasticity,bump_penalty)
-        
+        stochasticity = 0.1
+        bump_penalty = -10
+        return EnvMaze(char_grid, stochasticity, bump_penalty)
+
     else:
-        raise LookupError('Cannot find Environment with name "'+env_name+'"')
-    
-    
+        raise LookupError(
+            'Cannot find Environment with name "' + env_name + '"')
